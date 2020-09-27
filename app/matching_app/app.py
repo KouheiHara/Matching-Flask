54264aa57a49086d3228835387b77a6bc076a73d
kouheihara <kouhei.ocp117@gmail.com>
2020-09-28 03:10:12 +0900

    APIはすべてPOST対応に変更

--- app/matching_app/app.py
+++ app/matching_app/app.py
@@ -1,12 +1,14 @@
 import os
 from flask import Flask
 from logging import StreamHandler, FileHandler, Formatter
+from flask_jwt_extended import JWTManager  # noqa
 from flask.logging import default_handler
 from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
 api = Api(app)
+jwt = JWTManager(app)
 
 environment = "development"
 if os.getenv('FLASK_ENV') is not None:
